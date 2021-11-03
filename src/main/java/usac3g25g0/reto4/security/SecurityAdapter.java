c37a78ffa4a76c5dbdadcbabcdcbc933c081ccbc
Andrés Durán C <aduran9@hotmail.com>
2021-11-02 21:14:02 -0500

    Corrección del SecurityAdapter Reto5

--- src/main/java/usac3g25g0/reto4/security/SecurityAdapter.java
+++ src/main/java/usac3g25g0/reto4/security/SecurityAdapter.java
@@ -1,9 +1,8 @@
 package usac3g25g0.reto4.security;
 
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class SecurityAdapter extends WebSecurityConfigurerAdapter {
+public class SecurityAdapter {
 
 }
--- src/main/java/usac3g25g0/reto4/service/ReservationService.java
+++ src/main/java/usac3g25g0/reto4/service/ReservationService.java
@@ -17,7 +17,7 @@ public interface ReservationService {
     public Reservation crearReservacion(Reservation reservacion);
     public boolean borrarReservacion(Integer id);
     public Reservation actualizaReservacion(Reservation reservacion);
-    public List<Reservation> getReservationByStatus(String status);
+//    public List<Reservation> getReservationByStatus(String status);
     public ReservationStatus getReservationStatusReport();
     public List<Reservation> getReservationPeriod(String dateOne, String dateTwo);
     public List<CountClient> getTopClients();
